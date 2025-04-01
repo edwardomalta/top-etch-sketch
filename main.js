173d0ebf1bfef9e7a1808f28f167a1ff4b2a4a4c
Edwardo Malta <edwardoalta@gmail.com>
2025-04-01 00:28:58 -0600

    Line function implemented

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 const container = document.querySelector(".container");
 
-function addSquare() {
+function addSquare(line) {
     const square = document.createElement("div");
     square.classList.add("square");
-    container.appendChild(square);
+    line.appendChild(square);
 }
 
-for (let i = 0; i < 16; i++) {
-    addSquare();
+function createLine(){
+    const line = document.createElement("div");
+    for (let i = 0; i < 16; i++) {
+        addSquare(line);
+    }
+    container.appendChild(line);
 }
+
+createLine();
